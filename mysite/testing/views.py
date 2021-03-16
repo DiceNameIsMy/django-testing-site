0bcc26171499a44dbc35d704b817c084f3368a80
Turar Nurdaulet <dicenameismy>
2021-03-16 18:08:01 +0600

    views for main page and all questions page

--- mysite/testing/views.py
+++ mysite/testing/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+
+from .models import Questions, Tests
 
 # Create your views here.
+class AllQuestionsView(generic.ListView):
+    template_name = 'testing/all_questions.html'
+    context_object_name = 'questions_list'
+
+    def get_queryset(self):
+        return Questions.objects.all()
+
+def MainPage(request):
+    return(HttpResponse('You are in the main page.'))
