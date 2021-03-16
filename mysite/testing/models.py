fb20a966c8faef7573677f94cf959bbe1d842f0d
Turar Nurdaulet <dicenameismy>
2021-03-16 18:37:41 +0600

    in model 'tests' was deleted questions_id_csv in model 'questions' was created test_id it's much better alternative that works better and it's easier to understand.

--- mysite/testing/models.py
+++ mysite/testing/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Tests(models.Model):
+    test_name = models.CharField(max_length=40)
+    test_description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.test_name
+
+
 class Questions(models.Model):
+    test_id = models.ForeignKey(Tests, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
     choice1 = models.CharField(max_length=100)
     choice2 = models.CharField(max_length=100)
@@ -10,12 +19,3 @@ class Questions(models.Model):
 
     def __str__(self):
         return self.question_text
-
-
-class Tests(models.Model):
-    test_name = models.CharField(max_length=40)
-    test_description = models.CharField(max_length=200)
-    questions_id_csv = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.test_name
