e84a7e738fe3927ba4a6503383d0f501b7537725
Turar Nurdaulet <dicenameismy>
2021-03-17 21:53:18 +0600

    reworked all questions view. now its done

--- mysite/testing/views.py
+++ mysite/testing/views.py
@@ -2,15 +2,35 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse
 
-from .models import Question, Test
+from .models import Test, Question, Answer
 
 # Create your views here.
 class AllQuestionsView(generic.ListView):
     template_name = 'testing/all_questions.html'
-    context_object_name = 'questions_list'
+    context_object_name = 'data'
+
 
     def get_queryset(self):
-        return Question.objects.all()
+        questions = []
+        answers = {}
+
+        for answer in Answer.objects.all():
+            if answer.question.text in answers:
+                answers[answer.question.text].append(answer.text)
+            else:
+                answers[answer.question.text] = [answer.text,]
+                questions.append(answer.question)
+        print(answers)
+        return answers
+
+
+class TestsView(generic.ListView):
+    template_name = 'testing/tests.html'
+    context_object_name = 'tests'
+
+    def get_queryset(self):
+        return Test.objects.all()
+
 
 def MainPage(request):
     return(HttpResponse('You are in the main page.'))
