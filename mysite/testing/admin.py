8a19ce359cc0d60ab015d86fdf729c0e594f1316
Turar Nurdaulet <dicenameismy>
2021-03-17 10:53:39 +0600

    now in admin site you can change the whole test in 1 page

--- mysite/testing/admin.py
+++ mysite/testing/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
+
 from .models import Questions, Tests
 # Register your models here.
 
-admin.site.register(Questions)
-admin.site.register(Tests)
+class QuestionsInline(admin.StackedInline):
+    model = Questions
+    extra = 10
+
+class TestsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields': ['test_name', 'test_description',]}),
+    ]
+    inlines = [QuestionsInline]
+
+admin.site.register(Tests, TestsAdmin)
 
