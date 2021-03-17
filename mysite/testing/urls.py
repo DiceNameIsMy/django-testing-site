8e45615b43cd08b39707c62eede2027ac127c959
Turar Nurdaulet <dicenameismy>
2021-03-17 21:52:48 +0600

    created path tests

--- mysite/testing/urls.py
+++ mysite/testing/urls.py
@@ -6,4 +6,5 @@ app_name = 'testing'
 urlpatterns = [
     path('', views.MainPage, name='Main page'),
     path('questions/', views.AllQuestionsView.as_view(), name='All questions'),
+    path('tests/', views.TestsView.as_view(), name='Tests'),
 ]
