3bdaba1b87ecd22eb5fabe7238a2058ff6cedb7d
Turar Nurdaulet <dicenameismy>
2021-03-17 21:53:54 +0600

    changed base path fer testing app

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('testing/', include('testing.urls')),
+    path('', include('testing.urls')),
     path('admin/', admin.site.urls),
 ]
