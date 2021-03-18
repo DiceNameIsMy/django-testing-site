fbce8c112d0a843ca14bc90bb9587187d3f1b697
Turar Nurdaulet <dicenameismy>
2021-03-18 17:50:12 +0600

    added testing process and main menu path

--- mysite/testing/urls.py
+++ mysite/testing/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'testing'
 urlpatterns = [
     path('', views.MainPage, name='Main page'),
+    path('tests/<int:pk>/testing/', views.TestingUser, name='Testing'),
     path('questions/', views.AllQuestionsView.as_view(), name='All questions'),
     path('tests/', views.TestsView.as_view(), name='Tests'),
-    path('reg/', views.RegisterUser, name='Sign up')
+    path('reg/', views.RegisterUser, name='Sign up'),
 ]
