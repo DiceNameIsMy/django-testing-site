743876f93bfc9c9ca29133c5b4fab16f4c35335a
Turar Nurdaulet <dicenameismy>
2021-03-20 14:55:35 +0600

    changed urls for testing

--- testing/urls.py
+++ testing/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'testing'
 urlpatterns = [
     path('', views.MainPage, name='Main page'),
-    path('tests/<int:pk>/testing/', views.TestingUser, name='Testing'),
+    path('tests/<int:pk>/testing/', views.TestingPage, name='Testing page'),
+    path('tests/<int:pk>/testing/<int:q_pk>', views.TestingUser, name='Testing process'),
     path('questions/', views.AllQuestionsView.as_view(), name='All questions'),
     path('tests/', views.TestsView.as_view(), name='Tests'),
     path('signup/', views.RegisterUser, name='Sign up'),
