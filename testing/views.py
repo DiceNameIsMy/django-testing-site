2269a8548502c85f54c4ec279118bd20d7bb3773
Turar Nurdaulet <dicenameismy>
2021-03-20 16:49:21 +0600

    made testing page and it works! (Need some upgrades like correctly writing results and showing them to user)

--- testing/views.py
+++ testing/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import Context
@@ -44,11 +45,35 @@ def TestingPage(request, pk):
         }
         return render(request, 'testing/testing.html', context)
 
-def TestingUser(request, pk, q_pk):
-    if request.method == 'GET':
-        test = Test.objects.get(pk=pk) 
-        question = Question.objects.get(pk=q_pk)
 
+def TestingUser(request, pk, q_pk):
+    test = Test.objects.get(pk=pk) 
+    question = Question.objects.get(pk=q_pk, test=test)
+
+    if request.method == "POST":
+        # contains pk's of correct answers
+        correct_answers_pk = [str(answer.pk) for answer in Answer.objects.filter(question=question, is_correct=True)] 
+        # contains pk's of posted answers
+        posted_answers = request.POST.getlist('answers')
+
+        for answer in correct_answers_pk: 
+            if answer in posted_answers: #if correct answer is in posted answers delete it from there
+                posted_answers.remove(answer)
+            else: # if correct answer is not in posted answers break loop and head to the next question
+                break
+        else: #if all answers was correct
+            if posted_answers == []: #if no answers left(all correct answers was deleted)
+                user = User.objects.get(username=request.user.username)
+                UserTests.objects.get(user=user).score += 1 # add one point to user score
+
+        try:
+            link = Question.objects.get(pk=q_pk+1, test=test)
+            return HttpResponseRedirect(f'{link.pk}')
+        except Exception:
+            return HttpResponseRedirect('/tests/1/completed')
+
+    elif request.method == 'GET':
+        question = Question.objects.get(pk=q_pk, test=test)
         context = {
             'test_name': test.name,
             'question': question,
@@ -68,6 +93,8 @@ def RegisterUser(request):
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return HttpResponse('Registration is success!')
+        else:
+            return HttpResponse('Your form is invalid!')
     else:
         form = UserCreationForm()
         return render(request, 'testing/signup.html', {'form': form,})
@@ -76,7 +103,7 @@ def RegisterUser(request):
 def LoginUser(request):
     if request.method == "POST":
         user = authenticate(username=request.POST['username'], password=request.POST['password'])
-        if user is not None:
+        if user is not 'AnonymousUser':
             return HttpResponseRedirect('/')
         else:
             return render(request, 'testing/signin.html', {'message': 'Please enter the correct username and password.'})
