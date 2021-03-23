937ed44887d40ba0677749379a7c7edcc9dfd461
Turar Nurdaulet <dicenameismy>
2021-03-23 17:49:36 +0600

    comment paths for future realisation and deleted all questions view

--- testing/urls.py
+++ testing/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('tests/<int:pk>/', views.TestingPageView.as_view(), name='Testing page'),
     path('tests/<int:pk>/completed', views.TestCompletedView.as_view() , name='Test was completed'),
     path('tests/', views.TestsView.as_view(), name='Tests list'),
-    path('questions/', views.AllQuestionsView.as_view(), name='All questions'),
     path('signup/', views.RegisterUserView.as_view(), name='Sign up'),
     path('signin/', views.LoginUserView.as_view(), name='Sign in'),
+    # path('logout/', views.LogoutUserView.as_view(), name='Log out'),
+    # path('delete/', views.DeleteUserView.as_view(), name='Delete User'),
 ]
