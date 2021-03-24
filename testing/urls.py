b94bc857daa9a476927fc2707d5628b0ab97b824
Turar Nurdaulet <dicenameismy>
2021-03-24 20:44:40 +0600

    turn on logout path

--- testing/urls.py
+++ testing/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('tests/', views.TestsView.as_view(), name='Tests list'),
     path('signup/', views.RegisterUserView.as_view(), name='Sign up'),
     path('signin/', views.LoginUserView.as_view(), name='Sign in'),
-    # path('logout/', views.LogoutUserView.as_view(), name='Log out'),
+    path('logout/', views.LogoutUserView.as_view(), name='Log out'),
     # path('delete/', views.DeleteUserView.as_view(), name='Delete User'),
 ]
