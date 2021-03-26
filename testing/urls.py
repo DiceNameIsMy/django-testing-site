7d8e9313146d241b732ab45d85920ddd8ed557ae
Turar Nurdaulet <dicenameismy>
2021-03-26 18:34:45 +0600

    rename TestingUserView to TestingProcessView

--- testing/urls.py
+++ testing/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'testing'
 urlpatterns = [
     path('', views.MainPageView.as_view(), name='Main page'),
-    path('tests/<int:pk>/testing/<int:q_pk>', views.TestingUserView.as_view(), name='Testing process'),
+    path('tests/<int:pk>/testing/<int:q_pk>', views.TestingProcessView.as_view(), name='Testing process'),
     path('tests/<int:pk>/', views.TestingPageView.as_view(), name='Testing page'),
     path('tests/<int:pk>/completed', views.TestCompletedView.as_view() , name='Test was completed'),
     path('tests/', views.TestsView.as_view(), name='Tests list'),
