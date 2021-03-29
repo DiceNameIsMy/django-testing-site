52c42788b5c382ca861b4060608d306b45f7df5e
Turar Nurdaulet <dicenameismy>
2021-03-29 09:45:46 +0600

    new admin panel for groups

--- testing/admin.py
+++ testing/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from .models import Test, Question, Answer, UserTest
+from .models import Test, Question, Answer, UserTest, TestGroup
 # Register your models here.
 
 class AnswersInline(admin.TabularInline):
@@ -13,9 +13,14 @@ class QuestionsInline(admin.StackedInline):
     model = Question
     extra = 1
 
+class TestsInline(admin.StackedInline):
+    model = Test
+    extra = 0
+
 class UserTestsInline(admin.TabularInline):
     model = UserTest
     extra = 0
+
 
 class QuestionsAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -23,11 +28,20 @@ class QuestionsAdmin(admin.ModelAdmin):
     ]
     inlines = [AnswersInline]
 
+
 class TestsAdmin(admin.ModelAdmin):
     fieldsets = [
-        ('Name', {'fields': ['name', 'description', 'questions_amount']}),
+        ('Name', {'fields': ['name', 'description', 'group', 'questions_amount']}),
     ]
     inlines = [QuestionsInline]
+
+
+class TestGroupsAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Name', {'fields': ['name', 'description']})
+    ]
+    inlines = [TestsInline]
+
 
 class UserAdmin(BaseUserAdmin):
     inlines = (UserTestsInline,)
@@ -36,6 +50,8 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(Question, QuestionsAdmin)
 
 admin.site.register(Test, TestsAdmin)
+
+admin.site.register(TestGroup, TestGroupsAdmin)
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
