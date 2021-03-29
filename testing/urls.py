50c156f55e5c60282e4d45e9f808920d284ba8f2
Turar Nurdaulet <dicenameismy>
2021-03-29 10:19:16 +0600

    change navigation links

--- testing/urls.py
+++ testing/urls.py
@@ -5,10 +5,11 @@ from . import views
 app_name = 'testing'
 urlpatterns = [
     path('', views.MainPageView.as_view(), name='Main page'),
-    path('tests/<int:t_pk>/testing/<int:q_pk>', views.TestingProcessView.as_view(), name='Testing process'),
-    path('tests/<int:t_pk>/', views.TestingPageView.as_view(), name='Testing page'),
-    path('tests/<int:t_pk>/completed', views.TestCompletedView.as_view() , name='Test was completed'),
-    path('tests/', views.TestsView.as_view(), name='Tests list'),
+    path('tests/<slug:group_slug>/<int:t_pk>/testing/<int:q_pk>', views.TestingProcessView.as_view(), name='Testing process'),
+    path('tests/<slug:group_slug>/<int:t_pk>/', views.TestingPageView.as_view(), name='Testing page'),
+    path('tests/<slug:group_slug>/<int:t_pk>/completed', views.TestCompletedView.as_view() , name='Test was completed'),
+    path('tests/<slug:group_slug>/', views.GroupTestsView.as_view(), name='Tests in group'),
+    path('groups/', views.GroupOfTestsView.as_view(), name='Test groups list'),
     path('signup/', views.RegisterUserView.as_view(), name='Sign up'),
     path('signin/', views.LoginUserView.as_view(), name='Sign in'),
     path('logout/', views.LogoutUserView.as_view(), name='Log out'),
