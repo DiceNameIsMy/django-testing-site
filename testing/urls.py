13d1e1cc8635f6ce0d8d844e7d57f41aab69e5ad
Turar Nurdaulet <dicenameismy>
2021-04-17 15:20:37 +0600

    refactoring

--- testing/urls.py
+++ testing/urls.py
@@ -1,18 +1,19 @@
 from django.urls import path
 
-from . import views
+from .views import *
 
 app_name = 'testing'
 urlpatterns = [
-    path('', views.MainPageView.as_view(), name='Main page'),
-    path('tests/<slug:group_slug>/<int:t_pk>/testing/<int:q_pk>', views.TestingProcessView.as_view(), name='Testing process'),
-    path('tests/<slug:group_slug>/<int:t_pk>/', views.TestingPageView.as_view(), name='Testing page'),
-    path('tests/<slug:group_slug>/<int:t_pk>/completed', views.TestCompletedView.as_view() , name='Test was completed'),
-    path('tests/<slug:group_slug>/', views.GroupTestsView.as_view(), name='Tests in group'),
-    path('groups/', views.GroupsOfTestsView.as_view(), name='Test groups list'),
-    path('signup/', views.RegisterUserView.as_view(), name='Sign up'),
-    path('signin/', views.LoginUserView.as_view(), name='Sign in'),
-    path('logout/', views.LogoutUserView.as_view(), name='Log out'),
-    path('user/tests/', views.ManageTestsView.as_view(), name='Manage tests'),
-    path('user/tests/<int:t_pk>', views.TestDetailView.as_view(), name='Test detail'),
+    path('', MainPageView.as_view(), name='Main page'),
+    path('tests/<slug:group_slug>/<int:t_pk>/testing/<int:q_pk>', TestingProcessView.as_view(), name='Testing process'),
+    path('tests/<slug:group_slug>/<int:t_pk>/', TestingPageView.as_view(), name='Testing page'),
+    path('tests/<slug:group_slug>/<int:t_pk>/completed', TestCompletedView.as_view() , name='Test was completed'),
+    path('tests/<slug:group_slug>/', GroupTestsView.as_view(), name='Tests in group'),
+    path('groups/', GroupsOfTestsView.as_view(), name='Test groups list'),
+    path('signup/', RegisterUserView.as_view(), name='Sign up'),
+    path('signin/', LoginUserView.as_view(), name='Sign in'),
+    path('logout/', LogoutUserView.as_view(), name='Log out'),
+    path('user/', UserPageView.as_view(), name='User Page'),
+    path('user/tests/', ManageTestsView.as_view(), name='Manage tests'),
+    path('user/tests/<int:t_pk>', TestDetailView.as_view(), name='Test detail'),
 ]
